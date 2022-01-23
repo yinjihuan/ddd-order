60c85c45922fa9fd3ed3bf0f0f321f4ee4ce6fcb
jihuan.yin <jihuan900@126.com>
2022-01-23 11:32:22 +0800

    提交模块基本结构

---
+++ ddd-order-start/src/main/java/com/cxytiandi/ddd/order/OrderApplicationStart.java
@@ -0,0 +1,21 @@
+package com.cxytiandi.ddd.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @作者 尹吉欢
+ * @个人微信 jihuan900
+ * @微信公众号 猿天地
+ * @GitHub https://github.com/yinjihuan
+ * @作者介绍 http://cxytiandi.com/about
+ * @时间 2022-01-23 11:30
+ */
+@SpringBootApplication
+public class OrderApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplicationStart.class, args);
+    }
+}
